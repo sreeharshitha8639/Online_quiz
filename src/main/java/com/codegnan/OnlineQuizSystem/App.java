0360d3f8f383e326cd69d92e696c1a3209cad35a
sreeharshitha8639 <harshithayelukur@gmail.com>
2026-02-08 15:34:40 +0530

    Add files via upload

---
+++ src/main/java/com/codegnan/OnlineQuizSystem/App.java
@@ -0,0 +1,13 @@
+package com.codegnan.OnlineQuizSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
